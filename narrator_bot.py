4b48b3f7897b7169539b0f7e9e4224993c53e887
Aline Abler <aline.abler@beekeeper.io>
2021-03-15 13:31:16 +0100

    hyype

--- narrator_bot.py
+++ narrator_bot.py
@@ -117,6 +117,9 @@ def on_champd_lyrics(bot, message):
 def on_trombone(bot, message):
     send_voice(bot, message, 'trombone')
 
+def on_hype(bot, message):
+    send_voice(bot, message, 'hypetrain')
+
 def on_potato(bot, message):
     send_voice(bot, message, 'chummypotato')
 
@@ -208,6 +211,7 @@ def init_sound_files(bot):
         sound_files['champd'] = bot.sdk.files.upload_file_from_path("media/champd.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
         sound_files['badum'] = bot.sdk.files.upload_file_from_path("media/badumdish.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
         sound_files['trombone'] = bot.sdk.files.upload_file_from_path("media/sadtrombone.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
+        sound_files['hypetrain'] = bot.sdk.files.upload_file_from_path("media/hypetrain.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
         sound_files['chummypotato'] = bot.sdk.files.upload_file_from_path("media/chummypotato.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
         sound_files['dishwasher'] = bot.sdk.files.upload_file_from_path("media/dishwasher.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
         sound_files['migros'] = bot.sdk.files.upload_file_from_path("media/migros.opus", upload_type=FILE_UPLOAD_TYPE_VOICE)
@@ -220,6 +224,7 @@ def init_sound_files(bot):
         sound_files['champd-ins'] = open("media/champd-ins.opus", 'rb')
         sound_files['badum'] = open("media/badumdish.opus", 'rb')
         sound_files['trombone'] = open("media/sadtrombone.opus", 'rb')
+        sound_files['hypetrain'] = open("media/hypetrain.opus", 'rb')
         sound_files['chummypotato'] = open("media/chummypotato.opus", 'rb')
         sound_files['dishwasher'] = open("media/dishwasher.opus", 'rb')
         sound_files['migros'] = open("media/migros.opus", 'rb')
@@ -248,6 +253,9 @@ def main(options):
     bot.add_handler(CommandHandler('champdup', on_champd, **kwargs))
     bot.add_handler(CommandHandler('champdl', on_champd_lyrics, **kwargs))
     bot.add_handler(CommandHandler('trombone', on_trombone, **kwargs))
+    bot.add_handler(CommandHandler('hype', on_hype, **kwargs))
+    bot.add_handler(CommandHandler('train', on_hype, **kwargs))
+    bot.add_handler(CommandHandler('hypetrain', on_hype, **kwargs))
     bot.add_handler(CommandHandler('potato', on_potato, **kwargs))
     bot.add_handler(CommandHandler('dishwasher', on_dishwasher, **kwargs))
     bot.add_handler(CommandHandler('sadtrombone', on_trombone, **kwargs))
